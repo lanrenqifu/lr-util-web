6afe0e2144317bba172adb4169cd4082b3a5d896
lanrenqifu <lanrenqifu@qq.com>
2016-08-16 22:39:20 +0800

    complete dev

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * @file 模块入口
+ * @type {exports}
+ */
+module.exports.define = require('./lib/class');
+module.exports.dispatcher = require('./lib/events');
+module.exports.util = require('./lib/util');
